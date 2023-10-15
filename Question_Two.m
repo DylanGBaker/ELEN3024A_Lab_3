486e4503c0fa65809ef1642f4be4a0e43eb2383f
DylanGBaker <skaduvee32@gmail.com>
2023-10-16 00:44:22 +0200

    Implemented Question 2

--- Question_Two.m
+++ Question_Two.m
@@ -10,12 +10,10 @@ for i = -15:1:25
     noise_array = awgn(Tx, i, "measured");
     Rx_with_noise = Tx + noise_array;
     snr_array(count) = i;
-    demodulated_symbols = convertSymbolsToBits(Rx_with_noise);
-    error = compareDemodulatedSymbolBits(bits, demodulated_symbols);
-    error_probability(count) = error/1000000;
+    demodulated_bits = convertSymbolsToBits(Rx_with_noise);
+    error = compareDemodulatedBits(bits, demodulated_bits);
+    error_probability(count) = error/data_rate;
     count  = count + 1;
 end
 
 plot(snr_array, error_probability);
-
-
